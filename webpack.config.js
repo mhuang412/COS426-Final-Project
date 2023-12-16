17c773e73825ff16e327b6656b52ae106013e0bf
Michelle Huang <mhuang412@gmail.com>
2023-12-15 21:53:35 -0500

    over this

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const buildPath = './build/';
 
 module.exports = {
-    entry: ['./dist/app.js'],
+    entry: ['./src/app.js'],
     output: {
         path: path.join(__dirname, buildPath),
         filename: '[name].[hash].js',
